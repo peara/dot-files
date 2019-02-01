428da41fb0695b4532636508b9e5f133632bf367
Vu Ngoc Quang <quang.vn@outlook.com>
2019-02-01 08:41:02 +0700

    fix bug in linking

--- dot-link.sh
+++ dot-link.sh
@@ -11,16 +11,20 @@ CONFS=(${TMUX[@]} ${TMUXINATOR[@]} ${ZSH[@]})
 for app in "${CONFS[@]}"; do
   for file in "${app[@]}"; do
     TNAME="${HOME}/${file}"
-    FNAME=${file#.}
+    FNAME="`pwd`/${file#.}"
     echo "Linking ${TNAME} to ${FNAME}"
     FTYPE=$(file -b "$TNAME")
     echo $FTYPE
     if [[ $FTYPE =~ "symbolic link" ]]; then
       echo "Skip symbolic link"
+    elif [[ -z $FTYPE ]]; then
+      echo "Setting not found. Checking for existence of application"
+      echo "If found, then create symlink"
+      echo "Else, do nothing or ask for installing"
     else
       echo "Creating backup for ${TNAME}"
       cp -f ${TNAME} ${TNAME}.bak
-      echo "Moving and creating symbolic link for ${TNAME}"
+      echo "Moving to ${FNAME} and creating symbolic link for ${TNAME}"
       mv ${TNAME} ${FNAME} && ln -s ${FNAME} ${TNAME}
     fi
   done
